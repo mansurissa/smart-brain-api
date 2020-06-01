5d157f34b48f0ac0badbe95a2596e86f63801c36
mansurissa <mansurissa6@gmail.com>
2020-06-01 10:19:14 +0200

    updating db connection

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-solid-08153',
         user: 'postgres',
-        password: 'pass2database',
+        password: '',
         database: 'smart_brain',
 
 
